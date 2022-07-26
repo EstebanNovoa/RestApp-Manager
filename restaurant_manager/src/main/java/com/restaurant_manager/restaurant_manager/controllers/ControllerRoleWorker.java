8e5ca436f012435797d63d5ac7b1a6873f303cbf
Henry <henry.bonilla@uptc.edu.co>
2022-07-26 18:19:40 -0500

    Worker reserve

--- restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/controllers/ControllerRoleWorker.java
+++ restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/controllers/ControllerRoleWorker.java
@@ -1,10 +1,32 @@
 package com.restaurant_manager.restaurant_manager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.restaurant_manager.restaurant_manager.models.orders.repository.OrderRepository;
 
 @Controller
 @RequestMapping ("/api/worker")
 public class ControllerRoleWorker {
     
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    //Orders list
+    @RequestMapping ("/orders/list/{id}")
+    public ResponseEntity<?> getOrdersList(@PathVariable Long id) {
+        return ResponseEntity.ok(orderRepository.findAllByWorker(id));
+    }
+
+    @RequestMapping ("/orders/list")
+    public ResponseEntity<?> getOrdersList(@RequestBody String email) {
+        return ResponseEntity.ok(orderRepository.findAllByWorker(email));
+    }
+
+
 }
--- restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/models/orders/repository/OrderRepository.java
+++ restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/models/orders/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.restaurant_manager.restaurant_manager.models.orders.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.restaurant_manager.restaurant_manager.models.orders.Order;
@@ -7,4 +9,8 @@ import com.restaurant_manager.restaurant_manager.models.orders.Order;
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
     long countByState(String state);
+
+    List<Order> findAllByWorker(Long id);
+
+    List<Order> findAllByWorker(String email);
 }
