feed535c7fa1e7d1ecc160d9f29c007c43fa874d
Henry Bonilla <duvan-bonilla@hotmail.com>
2022-07-27 17:12:44 -0500

    cors

--- restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/RestaurantManagerApplication.java
+++ restaurant_manager/src/main/java/com/restaurant_manager/restaurant_manager/RestaurantManagerApplication.java
@@ -10,4 +10,13 @@ public class RestaurantManagerApplication {
 		SpringApplication.run(RestaurantManagerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
